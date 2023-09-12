157f980a689b2e6627daa19d0a9113b3f1cd5314
Margaret Lesly <62084257+maiiless@users.noreply.github.com>
2023-09-11 23:05:44 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,9 @@ module.exports = {
         }), 
     ],
     devServer: {
-        historyApiFallback: true 
+        historyApiFallback: true ,
+        compress: true,
+          allowedHosts: 'all'
     }
 }
 
